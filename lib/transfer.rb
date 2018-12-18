b7494cd956993943e0ac58d93222d372c33845e7
Meisuleen Kased <meisuleen@gmail.com>
2018-12-18 04:01:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,10 +28,10 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status == "pending"
+    if @status == "complete"
       @receiver.balance -= @amount
       @sender.balance += @amount
-      @status = "complete"
+      @status = "reversed"
     end
   end
 
